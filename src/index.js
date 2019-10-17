687003efe7e3f8780ebfc184392e942fa2988c1a
learner006 <learner06@mail.ru>
2019-10-18 00:36:31 +0300

    feat: a solution!

--- src/index.js
+++ src/index.js
@@ -1,54 +1,87 @@
- function say(p_text) {
-	console.log(p_text);
-}
+const MORSE_TABLE = {
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
+};
 
-function getZerosForFactorial(p_facNum, p_isDoubleFac)
-{
-	// In fact there is a math problem
-	// Let a sequence a_k = k \forall k = 1..p_facNum
-    // or a sequence a_n = 1 * 3 * 5 * ... * p_facnum (even)
-    // or a sequence a_m = 2 * 4 * 6 * ... * p_facnum (odd)
-    // ---
-    // The amount of zeros equal the amount of 10 in these sequences
+function decode(p_expr) {
+	let out_DecodedExpr = '';
 
-    if (p_facNum == 0)
-    	p_facNum = 1;
-	
-	let isFactorialNumOdd = !(p_facNum % 2);
+	let morseNumericChars = p_expr.match(/.{10}/g);
+	let mapMorseChar = {
+		'10': '.',
+		'11': '-',
+		'**': ' '
+	};
 
-    
-    let out_Result = 0;
-
-    if (!p_isDoubleFac || (p_isDoubleFac && isFactorialNumOdd))
-    	out_Result = Math.floor(p_facNum/10);
-
-	return out_Result;
-}
-
-module.exports = function zeros(p_expression) {
-	let out_ZerosCount = 0;
-
-	let pretokenArr = p_expression.match(/\*?\d+!!?/g);
-
-	if (pretokenArr === null)
-		return 0;
-
-	
-	for(let preToken of Object.values(pretokenArr))
+	for(let morseNumericChar of Object.values(morseNumericChars))
 	{
-        // Example
-        // [ '55!!', '55', '!!', index: 0, input: '55!!', groups: undefined ]
+		//say(charCode);
+		let morseTraditionalChar = '';
 
-		let num_fac_pair = preToken.match(/(\d+)(!!?)/);
-		let num = num_fac_pair[1];
-		let isDoubleFac = num_fac_pair[1].length == 2;
+		let morseCodeNumericCodes = morseNumericChar.match(/.{2}/g);
 
-		let z = getZerosForFactorial(num,isDoubleFac);
-		if (z > 0)
-			out_ZerosCount = (out_ZerosCount == 0) ? z : out_ZerosCount * z;
+		for(let numCode of Object.values(morseCodeNumericCodes))
+		{           
+		    let dash_dot = mapMorseChar[numCode];
 
-		//say(num_fac_pair);
+		    if (dash_dot !== undefined)
+				morseTraditionalChar += dash_dot;
+
+			// it is an unusual numCode! ;-)
+			if (numCode == '**')
+				break;
+		}
+
+		// Let's look up a table for a char!
+		if (morseTraditionalChar == " ")
+			out_DecodedExpr += " "
+		else
+		{
+			out_DecodedExpr += MORSE_TABLE[morseTraditionalChar];
+		}
 	}
 
-	return out_ZerosCount;
+	return out_DecodedExpr;
+}
+
+module.exports = {
+    decode
+}
+
+function say(p_text) {
+	console.log(p_text);
 }
